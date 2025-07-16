3bf16956503c331a5fbcaf8e44cfbdcd7e7751c7
Ikar336 <karunos.2015@mail.ru>
2025-07-17 02:34:27 +0700

    Ð1 попытка

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,45 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDao userDao = new UserDaoHibernateImpl();
+
+        //Создание таблицы
+        userDao.createUsersTable();
+
+        //Добавление 4 пользователей
+        userDao.saveUser("Ivan", "Ivanov", (byte) 30);
+        userDao.saveUser("Petr", "Petrov", (byte) 25);
+        userDao.saveUser("Sidor", "Sidorov", (byte) 40);
+        userDao.saveUser("Anna", "Smirnova", (byte) 20);
+
+        //Получение всех пользователей
+        System.out.println("Список пользователей:");
+        List<User> users = userDao.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        //Удаление пользователя по ID (например, удалим Petr с id = 2)
+        userDao.removeUserById(2);
+
+        //Повторный вывод списка (должно быть 3 пользователя)
+        System.out.println("После удаления пользователя с id = 2:");
+        users = userDao.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        //Очистка таблицы
+        userDao.cleanUsersTable();
+
+        //Удаление таблицы
+        userDao.dropUsersTable();
     }
 }
